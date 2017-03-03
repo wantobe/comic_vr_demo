73d10f438bbc5020bbc205ab76f750787f6758ed
Wantobe <jining007@gmail.com>
2017-03-03 21:13:40 +0800

    修复一些bug，并增加一些功能，添加进度条，更新ui等

--- js/index.js
+++ js/index.js
@@ -1,22 +1,39 @@
 requirejs.config({
 	paths: {
-        text: 'require_plugins/text',
-        json: 'require_plugins/json',
+		text: 'require_plugins/text',
+		json: 'require_plugins/json',
 	},
-	shim:{
-		'krpano':{
-			init:function(){
-				var bgId = localStorage.getItem('bgId');
+	shim: {
+		'krpano': {
+			init: function() {
+				var bgId = localStorage.getItem('bgId'),
+					start_page = 0, //0-首页；1-静态；2-动态
+					preset_data = [{
+						xml: "xml/scenes/IndexScene.xml",
+						content_data: "../data/index_data.json"
+					}, {
+						xml: "xml/scenes/GalleryScene.xml",
+						content_data: "../data/chaoxiyou.json",
+						episodes:1
+					}, {
+						xml: "xml/scenes/MotionComicScene.xml",
+						content_data: "../data/child.json",
+						episodes:1
+					}];
+
 				embedpano({
-					xml: "xml/scenes/IndexScene.xml",
+					xml: preset_data[start_page].xml,
 					target: "pano",
-					html5: "auto",
+					html5: "only+webgl",
 					mobilescale: 1.0,
 					consolelog: true,
 					initvars: {
+						hasBg:typeof bgId == 'string',
 						bgId: bgId > 0 ? bgId : 0,
 						page: 0,
-						indexData: "../data/index_data.json"
+						indexData: preset_data[0].content_data,
+						data_: preset_data[start_page].content_data,
+						episodes_: preset_data[start_page].episodes
 					}
 				});
 			}
@@ -24,5 +41,4 @@ requirejs.config({
 	}
 });
 
-require(["krpano","text","json"]);
-
+require(["krpano", "text", "json"]);
